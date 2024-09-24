e1127cf296a6faca93474427bcbe7e7c0ce773c2
Sarlaac <sarlaac@gmail.com>
2024-09-24 11:57:42 -0600

    initial setup - almost tested

---
+++ bootstrap.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt install ssh python3 python3-pip python3-venv git -y
+python3 -m venv env --prompt homelab --upgrade-deps
+source env/bin/activate
+python3 -m pip install ansible ansible-lint
+ssh-keygen -t ed25519
+
+git clone https://github.com/Sarlaac/homelab.git
