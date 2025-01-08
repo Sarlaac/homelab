596844b1eda8bb7030de347315012d519ce5b931
Sarlaac <redacted>
2025-01-08 08:18:04 -0600

    python requests needed by ansible

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@
 sudo apt install ssh python3 python3-pip python3-venv git -y
 python3 -m venv env --prompt homelab --upgrade-deps
 source env/bin/activate
-python3 -m pip install ansible ansible-lint
+python3 -m pip install ansible ansible-lint requests
 ssh-keygen -t ed25519
 
 git clone https://github.com/Sarlaac/homelab.git
